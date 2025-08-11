3a54def92650ceb7c50fd4f3c75ec0c74b2d1b01
shadededen <shadenelsi418@gmail.com>
2025-08-11 17:31:10 -0300

    criacao do arquivo cep.php

---
+++ cep.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultar Cep</title>
+</head>
+<body>
+    <input name="cep" id="cep" type="text"/>
+    <button onclick="">Consultar Cep</button>
+    <script>
+        var cep = document.getElementById("cep")
+        </script>
+    
+</body>
+</html>
