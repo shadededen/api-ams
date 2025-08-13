c7110390204bd1186b275b1ed557a1bdfed594e8
shadededen <shadenelsi418@gmail.com>
2025-08-13 17:30:18 -0300

    continuamos a fazer o site de consultar CEP

--- cep.php
+++ cep.php
@@ -7,9 +7,26 @@
 </head>
 <body>
     <input name="cep" id="cep" type="text"/>
-    <button onclick="">Consultar Cep</button>
+    <button onclick="ConsultarCEP()">Consultar Cep</button>
+</br>
+    Endereço: <input type="text" id="endereco"/>
+</br>
+    Cidade: <input type="text" id="cidade"/>
+    </br>
+    Estado: <input type="text" id="estado"/>
+</br>
     <script>
         var cep = document.getElementById("cep")
+        function ConsultarCEP()
+        {
+            var cep = document.getElementById("cep")
+            let url = "https://viacep.com.br/ws/"+cep.value+"/json/"
+            fetch(url)
+            .then( resp => resp.json())
+            .then( dados => {
+                alert(dados.logradouro)
+            })
+        }
         </script>
     
 </body>
