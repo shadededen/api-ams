d8ca21f6dee408724cc8d8704b516faa7385e893
shadededen <shadenelsi418@gmail.com>
2025-08-20 16:59:23 -0300

    testando commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial</title>
+    <title>Página Iniciaal</title>
 </head>
 <body>
     <h1>Revisão Javascript</h1>
