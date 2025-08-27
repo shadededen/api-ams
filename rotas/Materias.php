3b4df17866aa4ec7ac4d53cec37c9b865794566e
shadededen <shadenelsi418@gmail.com>
2025-08-27 17:30:29 -0300

    Continuação do post.html e materias.php

--- rotas/Materias.php
+++ rotas/Materias.php
@@ -6,13 +6,30 @@ header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
 $metodoSolicitado = $_SERVER['REQUEST_METHOD'];
 $id = $_GET['id'] ?? NULL;
 
-switch{
+switch($metodoSolicitado){
     case "POST":
         $dados_recebidos = json_decode(file_get_contents("php://input"), true);
         break;
-    case "GET"
-        echo "Veio do navegador";
-        break;
+    case "GET":
+        $servidor = "localhost";
+        $usuario = "root";
+        $senha = "";
+        $banco = "aula_pw3";
+
+        $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+        $sql = "select * from materias";
+
+        $resultado = $conexao->query($sql);
+
+        $materias = [];
+        while($linha = $resultado->fetch_assoc()){
+            $materias[]=$linha;
+        }
+
+        echo json_encode($materias);
+        break;    
 }
+
 
 ?>
